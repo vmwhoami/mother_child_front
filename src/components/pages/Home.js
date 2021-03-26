bb5f80bb91f3de589efd9e7e71c3d07ff58f1e7c
vmwhoami <vmwhoami@gmail.com>
2021-03-26 23:10:03 +0200

    Add the loading heart

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Heart } from 'react-awesome-spinners';
 import Layout from '../Layout';
 import Doctor from '../comp/Doctor';
 
 const Home = () => {
   const state = useSelector((state) => state.doctorReducer);
+  const { loading } = state;
+  const heart = { color: '#000fff' };
   return (
     <Layout>
       <h1>Our doctors</h1>
       <div className="doctors">
-        {state.doctors.filter((doc) => doc)
+        {loading ? <Heart color={heart.color} /> : state.doctors.filter((doc) => doc)
           .map((doctor) => <Doctor key={doctor.id} doctor={doctor} />)}
       </div>
     </Layout>
--- src/redux/doctors/doctorReducer.js
+++ src/redux/doctors/doctorReducer.js
@@ -3,13 +3,14 @@ import { DOCTORS_SUCCESS, DOCTORS_FAILURE } from './doctorActTypes';
 const initial = {
   doctors: [],
   errors: false,
+  loading: true,
 };
 const doctorReducer = (state = initial, action) => {
   switch (action.type) {
     case DOCTORS_SUCCESS:
-      return { ...state, doctors: action.payload };
+      return { ...state, doctors: action.payload, loading: false };
     case DOCTORS_FAILURE:
-      return { ...state, errors: true };
+      return { ...state, errors: true, loading: false };
     default:
       return state;
   }
